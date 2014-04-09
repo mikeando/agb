722eb54f8a75b4ecaf45498bc44f779912d16162
Michael Anderson <drmikeando@gmail.com>
2014-04-10 07:39:27 +0800

    Moved fetch code out of test

--- include/anbgitbridge.h
+++ include/anbgitbridge.h
@@ -2,4 +2,5 @@ typedef struct ANBGitBridge ANBGitBridge;
 typedef struct ANBGitBridgeError ANBGitBridgeError;
 
 int anb_git_bridge_sync_files(ANBGitBridge* anbGitBridge, ANBGitBridgeError * error);
+int anb_git_bridge_fetch(ANBGitBridge * anbGitBridge, ANBGitBridgeError * error);
 
