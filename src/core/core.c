821cdbd93084ea827642d4c8529d830d41826a19
Michael Anderson <drmikeando@gmail.com>
2014-05-20 21:56:34 +0800

    Better NULL handling on error and xyz_free

--- src/core/core.c
+++ src/core/core.c
@@ -23,22 +23,26 @@ int agb_bridge_new( AGBCore ** anbGitBridge ) {
 }
 
 int agb_error_delete( AGBError * error ) {
+	if(!error) return 0;
 	free((char*)error->message);
 	free(error);
 	return 0;
 }
 
 int agb_bridge_delete( AGBCore * anbGitBridge ) {
+	if(!anbGitBridge) return 0;
 	free(anbGitBridge);
 	return 0;
 }
 
 int agb_merge_iterator_free( AGBMergeIterator * it ) { 
+	if(!it) return 0;
 	free(it);
 	return 0;
 }
 
 const char * agb_error_message( const AGBError * error) {
+	if(error==NULL) return "NULL passed to agb_error_message - can't give you a sane message";
 	return error->message;
 }
 
