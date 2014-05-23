d132b93a5cbd556ba3c474b0393a9f079155384f
Michael Anderson <drmikeando@gmail.com>
2014-05-23 09:26:00 +0800

    Changed test output slightly

--- tests/clar/print.h
+++ tests/clar/print.h
@@ -35,14 +35,18 @@ static void clar_print_error(int num, const struct clar_error *error)
 	fflush(stdout);
 }
 
+int sub_test_index =0;
+
 static void clar_print_ontest(const char *test_name, int test_number, int failed)
 {
-	printf("   [%03d] %s : %s\n", test_number, test_name, (failed)?"FAILED":"OK");
+	++sub_test_index;
+	printf("   [%03d] %s : %s\n", sub_test_index, test_name, (failed)?"FAILED":"OK");
 	fflush(stdout);
 }
 
 static void clar_print_onsuite(const char *suite_name, int suite_index)
 {
+	sub_test_index=0;
 	printf("[%02d] %s\n", suite_index, suite_name);
 }
 
